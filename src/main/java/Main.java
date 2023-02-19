f71c73bbad871b42d24ffcf121de475f80d71206
jafet <jafetpintle@gmail.com>
2023-02-18 22:40:53 -0600

    UI main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,28 @@
 import io.github.cdimascio.dotenv.Dotenv;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        boolean flag = true;
+        String[] botones = {"1. ver gatos", "2. salir"};
 
+        do {
+
+            String opcion = (String) JOptionPane.showInputDialog(null, "Gatitos Java", "Menu principal", JOptionPane.INFORMATION_MESSAGE, null, botones, botones[0]);
+
+            switch (opcion) {
+                case"1. ver gatos" :
+
+                    continue;
+                case"2. salir":
+                    flag = false;
+                    break;
+                default:
+            }
+
+        } while (flag);
     }
-
-
-
 
 }
